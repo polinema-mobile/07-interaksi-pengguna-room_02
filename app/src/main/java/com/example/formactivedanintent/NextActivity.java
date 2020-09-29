b97c37b43521653e896c4006f62dd3b8f9c9b067
nuruslaily <nuruslaily88@gmail.com>
2020-09-30 00:45:21 +0700

    renamepackage

---
+++ app/src/main/java/com/example/formactivedanintent/NextActivity.java
@@ -0,0 +1,29 @@
+package com.example.formactivedanintent;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next_activity);
+
+        Bundle b = getIntent().getExtras();
+        //membuat obyek dari widget textview
+        TextView nama = (TextView) findViewById(R.id.hasil_nama);
+        TextView nim = (TextView) findViewById(R.id.hasil_nim);
+        TextView tanggalLahir = (TextView) findViewById(R.id.hasil_tanggalLahir);
+        TextView jeniskelamin = (TextView) findViewById(R.id.hasil_jk);
+        TextView jurusan = (TextView) findViewById(R.id.hasil_jurusan);
+        //menset nilai dari widget textview
+        nama.setText(b.getCharSequence("nama"));
+        nim.setText(b.getCharSequence("nim"));
+        tanggalLahir.setText(b.getCharSequence("tanggalLahir"));
+        jeniskelamin.setText(b.getCharSequence("jeniskelamin"));
+        jurusan.setText(b.getCharSequence("jurusan"));
+
+    }
+}
