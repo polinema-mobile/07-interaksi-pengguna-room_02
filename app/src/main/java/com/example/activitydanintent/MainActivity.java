fa63c23e448a95a015cb687e58d92ffc0b67eaef
nuruslaily <nuruslaily88@gmail.com>
2020-09-29 23:29:40 +0700

    nextactivity

--- app/src/main/java/com/example/activitydanintent/MainActivity.java
+++ app/src/main/java/com/example/activitydanintent/MainActivity.java
@@ -1,63 +1,131 @@
-<?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-        xmlns:tools="http://schemas.android.com/tools"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        tools:context=".MainActivity">
+package com.example.activitydanintent;
+import androidx.appcompat.app.AppCompatActivity;
 
-<LinearLayout
-        android:layout_width="match_parent"
-                android:layout_height="wrap_content"
-                android:orientation="vertical">
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
-<EditText
-            android:id="@+id/edtNama"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"
-                    android:hint="Nama Mahasiswa"/>
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-<EditText
-            android:id="@+id/edtNim"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"
-                    android:hint="Nomor Induk Mahasiswa"/>
+public class MainActivity extends AppCompatActivity {
+    EditText nama, nim, tanggalLahir;
+    Spinner jurusan;
+    RadioGroup jeniskelamin;
+    RadioButton lakilaki, perempuan;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private String gender;
 
-<EditText
-            android:id="@+id/edtTanggalLahir"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"
-                    android:inputType="date"
-                    android:hint="Tanggal Lahir"/>
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-<RadioGroup
-            android:id="@+id/rg_jeniskelamin"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content">
+        final EditText nama = (EditText) findViewById(R.id.edtNama);
+        final EditText nim = (EditText) findViewById(R.id.edtNim);
 
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        tanggalLahir = (EditText) findViewById(R.id.edtTanggalLahir);
+        tanggalLahir.setInputType(InputType.TYPE_NULL);
+        tanggalLahir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog();
+            }
+        });
 
-<RadioButton
-                android:id="@+id/rb_laki"
-                        android:layout_width="match_parent"
-                        android:layout_height="wrap_content"
-                        android:checked="true"
-                        android:text="Laki Laki" />
+        RadioButton lakilaki = (RadioButton) findViewById(R.id.rb_laki);
+        RadioButton perempuan = (RadioButton) findViewById(R.id.rb_perempuan);
 
-<RadioButton
-                android:id="@+id/rb_perempuan"
-                        android:layout_width="match_parent"
-                        android:layout_height="wrap_content"
-                        android:text="Perempuan" />
-</RadioGroup>
+        jeniskelamin = (RadioGroup) findViewById(R.id.rg_jeniskelamin);
+        jeniskelamin.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.rb_laki:
+                        gender  = "Laki-Laki";
+                        break;
+                    case R.id.rb_perempuan:
+                        gender = "Perempuan";
+                        break;
+                }
+            }
+        });
 
-<Spinner
-            android:id="@+id/spinnerJurusan"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"/>
+        jurusan = (Spinner) findViewById(R.id.spinnerJurusan);
+        jurusan.setOnItemSelectedListener(new ItemSelectedListener());
 
-<Button
-            android:id="@+id/Simpan"
-                    android:text="SIMPAN DATA"
-                    android:layout_width="match_parent"
-                    android:layout_height="wrap_content"/>
-</LinearLayout>
-</RelativeLayout>
+        Button button = findViewById(R.id.Simpan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(new MainActivity(), NextActivity.class);
+
+                int id = jeniskelamin.getCheckedRadioButtonId();
+                RadioButton selectedGender = (RadioButton) findViewById(id);
+
+                //Menyisipkan tipe data String ke dalam obyek bundle
+                intent.putExtra("nama", nama.getText().toString());
+                intent.putExtra("nim", nim.getText().toString());
+                intent.putExtra("tanggalLahir", tanggalLahir.getText().toString());
+                intent.putExtra("jeniskelamin", gender);
+                intent.putExtra("jurusan", jurusan.getSelectedItem().toString());
+
+                //memulai Activity kedua
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showDateDialog(){
+        //Calendar untuk mendapatkan tanggal sekarang
+        Calendar newCalendar = Calendar.getInstance();
+
+        //Initiate DatePicker dialog
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                //Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                //Set Calendar untuk menampung tanggal yang dipilih
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                //Update TextView dengan tanggal yang kita pilih
+                tanggalLahir.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        //Tampilkan DatePicker dialog
+        datePickerDialog.show();
+    }
+
+    public class ItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        //get strings of first item
+        String firstItem = String.valueOf(jurusan.getSelectedItem());
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            firstItem.equals(String.valueOf(jurusan.getSelectedItem())); {
+
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+}
