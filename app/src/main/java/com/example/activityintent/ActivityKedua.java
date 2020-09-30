cc0ee9e5ac3a92da46459f9c7138c83c41510125
nuruslaily <nuruslaily88@gmail.com>
2020-09-30 23:18:37 +0700

    parcelable masih error

--- app/src/main/java/com/example/activityintent/ActivityKedua.java
+++ app/src/main/java/com/example/activityintent/ActivityKedua.java
@@ -2,6 +2,7 @@ package com.example.activityintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -9,24 +10,52 @@ import org.w3c.dom.Text;
 
 public class ActivityKedua extends AppCompatActivity {
 
+    String Data_mhs;
+    String nama;
+    String nim;
+    String tgl;
+    String jk;
+    String jurusan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kedua);
 
+        //intent
         Bundle b = getIntent().getExtras();
-        //membuat obyek dari widget textview
-        TextView nama = (TextView) findViewById(R.id.edtNama);
-        TextView nim = (TextView) findViewById(R.id.edtNIM);
-        TextView tanggalLahir = (TextView) findViewById(R.id.edtTanggalLahir);
-        TextView jeniskelamin = (TextView) findViewById(R.id.edtJenisKelamin);
-        TextView jurusan = (TextView) findViewById(R.id.edtJurusan);
-        //menset nilai dari widget textview
-        nama.setText(b.getCharSequence("nama"));
-        nim.setText(b.getCharSequence("nim"));
-        tanggalLahir.setText(b.getCharSequence("tanggalLahir"));
-        jeniskelamin.setText(b.getCharSequence("jeniskelamin"));
-        jurusan.setText(b.getCharSequence("jurusan"));
+        //parcelable
+        Intent intent = getIntent();
+        MainActivity JenisData = intent.getParcelableExtra("Parcelable");
+        startActivity(intent);
+        String type = intent.getStringExtra(String.valueOf(JenisData));
+                if(type.equals("Percelable")){
+                nama = JenisData.getNama();
+                nim = JenisData.getNim();
+                tgl = JenisData.getTgl();
+                jk = JenisData.getJk();
+                jurusan = JenisData.getJurusan();
+                    nama = String.valueOf((TextView) findViewById(R.id.edtNama));
+                    nim = String.valueOf((TextView) findViewById(R.id.edtNIM));
+                    tgl = String.valueOf((TextView) findViewById(R.id.edtTanggalLahir));
+                    jk = String.valueOf((TextView) findViewById(R.id.edtJenisKelamin));
+                    jurusan = String.valueOf((TextView) findViewById(R.id.edtJurusan));
+                } else {
+                    //membuat obyek dari widget textview
+                    TextView nama = (TextView) findViewById(R.id.edtNama);
+                    TextView nim = (TextView) findViewById(R.id.edtNIM);
+                    TextView tanggalLahir = (TextView) findViewById(R.id.edtTanggalLahir);
+                    TextView jeniskelamin = (TextView) findViewById(R.id.edtJenisKelamin);
+                    TextView jurusan = (TextView) findViewById(R.id.edtJurusan);
+                    //menset nilai dari widget textview
+                    nama.setText(b.getCharSequence("nama"));
+                    nim.setText(b.getCharSequence("nim"));
+                    tanggalLahir.setText(b.getCharSequence("tanggalLahir"));
+                    jeniskelamin.setText(b.getCharSequence("jeniskelamin"));
+                    jurusan.setText(b.getCharSequence("jurusan"));
+                }
+
 
     }
+
 }
--- app/src/main/java/com/example/activityintent/MainActivity.java
+++ app/src/main/java/com/example/activityintent/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.text.InputType;
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,25 +21,39 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity  {
-    EditText nama, nim, tanggalLahir;
-    Spinner jurusan;
-    RadioGroup jeniskelamin;
-    RadioButton lakilaki, perempuan;
+public class MainActivity extends AppCompatActivity implements Parcelable {
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
     private String gender;
+    EditText nama;
+    EditText nim;
+    EditText tanggalLahir;
+    RadioButton lakilaki;
+    RadioButton perempuan;
+    RadioGroup jeniskelamin;
+    Spinner jurusan;
+    Button buttonIntent;
+    Button buttonPercelable;
+    String JenisData;
+    String Data_mhs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText nama = (EditText) findViewById(R.id.edtNama);
-        final EditText nim = (EditText) findViewById(R.id.edtNim);
+        nama = (EditText) findViewById(R.id.edtNama);
+        nim = (EditText) findViewById(R.id.edtNim);
+        tanggalLahir = (EditText) findViewById(R.id.edtTanggalLahir);
+        lakilaki = (RadioButton) findViewById(R.id.radioLakilaki);
+        perempuan = (RadioButton) findViewById(R.id.radioPerempuan);
+        jeniskelamin = (RadioGroup) findViewById(R.id.radioGroup);
+        jurusan = (Spinner) findViewById(R.id.listJurusan);
+        buttonIntent = findViewById(R.id.btnSimpanIntent);
+        buttonPercelable = findViewById(R.id.btnSimpanParcel);
+
 
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
-        tanggalLahir = (EditText) findViewById(R.id.edtTanggalLahir);
         tanggalLahir.setInputType(InputType.TYPE_NULL);
         tanggalLahir.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,17 +61,12 @@ public class MainActivity extends AppCompatActivity  {
                 showDateDialog();
             }
         });
-
-        RadioButton lakilaki = (RadioButton) findViewById(R.id.radioLakilaki);
-        RadioButton perempuan = (RadioButton) findViewById(R.id.radioPerempuan);
-
-        jeniskelamin = (RadioGroup) findViewById(R.id.radioGroup);
         jeniskelamin.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i) {
                     case R.id.radioLakilaki:
-                        gender  = "Laki-Laki";
+                        gender = "Laki-Laki";
                         break;
                     case R.id.radioPerempuan:
                         gender = "Perempuan";
@@ -63,33 +74,40 @@ public class MainActivity extends AppCompatActivity  {
                 }
             }
         });
-
-        jurusan = (Spinner) findViewById(R.id.listJurusan);
         jurusan.setOnItemSelectedListener(new ItemSelectedListener());
-
-        Button button = findViewById(R.id.btnSimpan);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonIntent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ActivityKedua.class);
-
+                Intent intent = new Intent(MainActivity.this, ActivityKedua.class);
                 int id = jeniskelamin.getCheckedRadioButtonId();
                 RadioButton selectedGender = (RadioButton) findViewById(id);
 
                 //Menyisipkan tipe data String ke dalam obyek bundle
+                intent.putExtra(JenisData,"Intent");
                 intent.putExtra("nama", nama.getText().toString());
                 intent.putExtra("nim", nim.getText().toString());
                 intent.putExtra("tanggalLahir", tanggalLahir.getText().toString());
-                intent.putExtra("jeniskelamin", gender);
+                intent.putExtra("jeniskelamin", selectedGender.getText().toString());
                 intent.putExtra("jurusan", jurusan.getSelectedItem().toString());
 
                 //memulai Activity kedua
                 startActivity(intent);
             }
         });
+        buttonPercelable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityKedua.class);
+                int id = jeniskelamin.getCheckedRadioButtonId();
+                RadioButton selectedGender = (RadioButton) findViewById(id);
+
+                intent.putExtra(JenisData,"Parcelable");
+                startActivity(intent);
+            }
+        });
     }
 
-    private void showDateDialog(){
+    private void showDateDialog() {
         //Calendar untuk mendapatkan tanggal sekarang
         Calendar newCalendar = Calendar.getInstance();
 
@@ -106,7 +124,7 @@ public class MainActivity extends AppCompatActivity  {
                 //Update TextView dengan tanggal yang kita pilih
                 tanggalLahir.setText(dateFormatter.format(newDate.getTime()));
             }
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
         //Tampilkan DatePicker dialog
         datePickerDialog.show();
@@ -118,7 +136,8 @@ public class MainActivity extends AppCompatActivity  {
         String firstItem = String.valueOf(jurusan.getSelectedItem());
 
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-            firstItem.equals(String.valueOf(jurusan.getSelectedItem())); {
+            firstItem.equals(String.valueOf(jurusan.getSelectedItem()));
+            {
 
             }
         }
@@ -129,4 +148,59 @@ public class MainActivity extends AppCompatActivity  {
         }
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable((Parcelable) this.nama, flags);
+        dest.writeParcelable((Parcelable) this.nim, flags);
+        dest.writeParcelable((Parcelable) this.tanggalLahir, flags);
+        dest.writeParcelable((Parcelable) this.jurusan, flags);
+        dest.writeParcelable((Parcelable) this.jeniskelamin, flags);
+        dest.writeParcelable((Parcelable) this.lakilaki, flags);
+        dest.writeParcelable((Parcelable) this.perempuan, flags);
+
+    }
+
+    public MainActivity(EditText nm, EditText nonim, EditText tglLahir, Spinner jrsn, RadioGroup jk) {
+        this.nama = nm;
+        this.nim = nonim;
+        this.tanggalLahir = tglLahir;
+        this.jurusan = jrsn;
+        this.jeniskelamin = jk;
+    }
+
+    public MainActivity() {
+
+    }
+
+    protected MainActivity(Parcel in) {
+        nama = in.readParcelable(EditText.class.getClassLoader());
+        nim = in.readParcelable(EditText.class.getClassLoader());
+        tanggalLahir = in.readParcelable(EditText.class.getClassLoader());
+        jurusan = in.readParcelable(Spinner.class.getClassLoader());
+        jeniskelamin = in.readParcelable(RadioGroup.class.getClassLoader());
+        lakilaki = in.readParcelable(RadioButton.class.getClassLoader());
+        perempuan = in.readParcelable(RadioButton.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<MainActivity> CREATOR = new Parcelable.Creator<MainActivity>() {
+        @Override
+        public MainActivity createFromParcel(Parcel source) {
+            return new MainActivity(source);
+        }
+
+        @Override
+        public MainActivity[] newArray(int size) {
+            return new MainActivity[size];
+        }
+    };
+    public String getNama(){ return String.valueOf(nama); }
+    public String getNim(){ return String.valueOf(nim); }
+    public String getTgl(){ return String.valueOf(tanggalLahir); }
+    public String getJk(){ return String.valueOf(jeniskelamin); }
+    public String getJurusan(){ return String.valueOf(jurusan); }
 }
